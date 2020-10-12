8f0da4efe27c6bf73679694dcf9ff2580c4b2c36
Hangell <rodrigo@hangell.org>
2020-10-12 19:44:14 -0300

    Api hoteis sem Banco de Dados

---
+++ resources/hotel.py
@@ -0,0 +1,72 @@
+from flask_restful import Resource, reqparse
+from models.hotel import HotelModel
+
+hoteis = [
+    {
+        'hotel_id': 'alpha',
+        'nome': 'Alpha Hotel',
+        'estrelas': 4.3,
+        'diaria': 420.34,
+        'cidade': 'Rio de Janeiro'
+    },
+    {
+        'hotel_id': 'bravo',
+        'nome': 'Bravo Hotel',
+        'estrelas': 4.4,
+        'diaria': 380.90,
+        'cidade': 'São Paulo'
+    },
+    {
+        'hotel_id': 'cappa',
+        'nome': 'Cappa Hotel',
+        'estrelas': 3.8,
+        'diaria': 250.60,
+        'cidade': 'Rio Grande do Sul'
+    }
+]
+
+class Hoteis(Resource):
+    def get(self):
+        return {'hoteis': hoteis}
+
+class Hotel(Resource):
+    argumentos = reqparse.RequestParser()
+    argumentos.add_argument('nome')
+    argumentos.add_argument('estrelas')
+    argumentos.add_argument('diaria')
+    argumentos.add_argument('cidade')
+
+    def find_hotel(hotel_id):
+        for hotel in hoteis:
+            if hotel['hotel_id'] == hotel_id:
+                return hotel
+        return None
+
+    def get(self, hotel_id):
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel:
+            return hotel
+        return {'message': 'Hotel not found'}, 404
+
+    def post(self, hotel_id):
+        dados = Hotel.argumentos.parse_args()
+        hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = hotel_objeto.json()
+        hoteis.append(novo_hotel)
+        return novo_hotel, 201
+
+    def put(self, hotel_id):
+        dados = Hotel.argumentos.parse_args()
+        hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = hotel_objeto.json()
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel:
+            hotel.update(novo_hotel)
+            return novo_hotel, 200 # OK
+        hoteis.append(novo_hotel)
+        return novo_hotel, 201 #criado
+
+    def delete(self, hotel_id):
+        global hoteis
+        hoteis = [hotel for hotel in hoteis if hotel['hotel_id'] != hotel_id]
+        return {'message': 'Hotel deleted'}
